a5518e1610a52cb000b7f1b57de2bbbd0a0d8a15
kevinpollet <pollet.kevin@gmail.com>
2019-03-24 17:36:33 +0400

    Add a cookie consent banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@
 /**
  * Main module
  */
-(function() {
+(function(tarte) {
+  // Docs select menu listener
   document.addEventListener("DOMContentLoaded", () => {
     const select = document.querySelector('select[id="navigation"]');
     if (select) {
@@ -11,4 +12,26 @@
       });
     }
   });
-})();
+
+  // Cookie consent banner
+  {{ if not .Site.IsServer }}
+    tarte.user.googletagmanagerId = "{{ .Site.Params.googleTagManagerID }}";
+    (tarte.job = tarte.job || []).push("googletagmanager");
+  {{ end }}
+
+  tarte.init({
+    privacyUrl: "" /* Privacy policy url */,
+    hashtag: "#tarteaucitron" /* Open the panel with this hashtag */,
+    cookieName: "tartaucitron" /* Cookie name */,
+    orientation: "bottom" /* Banner position (top - bottom) */,
+    showAlertSmall: false /* Show the small banner on bottom right */,
+    cookieslist: false /* Show the cookie list */,
+    adblocker: false /* Show a Warning if an adblocker is detected */,
+    AcceptAllCta: true /* Show the accept all button when highPrivacy on */,
+    highPrivacy: true /* Disable auto consent */,
+    handleBrowserDNTRequest: false /* If Do Not Track == 1, disallow all */,
+    removeCredit: true /* Remove credit link */,
+    moreInfoLink: true /* Show more info link */,
+    useExternalCss: true /* If false, the tarteaucitron.css file will be loaded */,
+  });
+})(tarteaucitron);
