f37b6c81a7bc5624eaf2e2fee6b94f53de423e7e
kevinpollet <pollet.kevin@gmail.com>
2019-03-29 14:59:48 +0400

    Force cookie banner language

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,23 +15,24 @@
 
   // Cookie consent banner
   {{ if not .Site.IsServer }}
+    window.tarteaucitronForceLanguage= "en";
     tarte.user.googletagmanagerId = "{{ .Site.Params.googleTagManagerID }}";
     (tarte.job = tarte.job || []).push("googletagmanager");
-  {{ end }}
 
-  tarte.init({
-    privacyUrl: "" /* Privacy policy url */,
-    hashtag: "#tarteaucitron" /* Open the panel with this hashtag */,
-    cookieName: "tartaucitron" /* Cookie name */,
-    orientation: "bottom" /* Banner position (top - bottom) */,
-    showAlertSmall: false /* Show the small banner on bottom right */,
-    cookieslist: false /* Show the cookie list */,
-    adblocker: false /* Show a Warning if an adblocker is detected */,
-    AcceptAllCta: true /* Show the accept all button when highPrivacy on */,
-    highPrivacy: true /* Disable auto consent */,
-    handleBrowserDNTRequest: false /* If Do Not Track == 1, disallow all */,
-    removeCredit: true /* Remove credit link */,
-    moreInfoLink: true /* Show more info link */,
-    useExternalCss: true /* If false, the tarteaucitron.css file will be loaded */,
-  });
+    tarte.init({
+      privacyUrl: "" /* Privacy policy url */,
+      hashtag: "#tarteaucitron" /* Open the panel with this hashtag */,
+      cookieName: "tartaucitron" /* Cookie name */,
+      orientation: "bottom" /* Banner position (top - bottom) */,
+      showAlertSmall: false /* Show the small banner on bottom right */,
+      cookieslist: false /* Show the cookie list */,
+      adblocker: false /* Show a Warning if an adblocker is detected */,
+      AcceptAllCta: true /* Show the accept all button when highPrivacy on */,
+      highPrivacy: true /* Disable auto consent */,
+      handleBrowserDNTRequest: false /* If Do Not Track == 1, disallow all */,
+      removeCredit: true /* Remove credit link */,
+      moreInfoLink: true /* Show more info link */,
+      useExternalCss: true /* If false, the tarteaucitron.css file will be loaded */,
+    });
+  {{ end }}
 })(tarteaucitron);
